599e3b0f14520b898a30fefe59aba697ecc81fcc
CNPMnhom05to04 <157586244+CNPMnhom05to04@users.noreply.github.com>
2025-07-28 17:08:00 +0700

    bản thử nghiệm

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,19 +1,34 @@
 import React from 'react';
 import Card from './Card';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination } from 'swiper/modules';
+
 import './Section.css';
+
 export default function Section({ title, items, onCardClick }) {
     return (
         <div className="section">
             <h2>{title}</h2>
-            <div className="items">
+
+            <Swiper
+                slidesPerView={1}
+                slidesPerGroup={1}
+                spaceBetween={16}
+                navigation
+                pagination={{ clickable: true }}
+                modules={[Navigation, Pagination]}
+            >
                 {items.map(item => (
-                    <Card
-                        key={item.id}
-                        {...item}
-                        onClick={() => onCardClick(item.id)}
-                    />
+                    <SwiperSlide key={item.id}>
+                        <Card {...item} onClick={() => onCardClick(item.id)} />
+                    </SwiperSlide>
                 ))}
-            </div>
+
+                {/* Nếu bạn muốn custom next/prev */}
+                <div className="swiper-button-prev"></div>
+                <div className="swiper-button-next"></div>
+                <div className="swiper-pagination"></div>
+            </Swiper>
         </div>
     );
 }
